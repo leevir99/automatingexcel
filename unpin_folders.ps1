db4d2a80dcccc636a3e5e4880896ded3b40eb8f5
LeeviR <46327802+leevir99@users.noreply.github.com>
2023-06-17 10:09:31 +0300

    Update unpin_folders.ps1

--- unpin_folders.ps1
+++ unpin_folders.ps1
@@ -25,3 +25,9 @@ Write-Host "Quick Access refreshed."
 powershell -ExecutionPolicy Bypass -File "%USERPROFILE%\Desktop\unpin_folders.ps1"
 pause
 
+
+----
+@echo off
+powershell -ExecutionPolicy Bypass -Command "& { $foldersToKeep = @('1. ASIAKIRJALAADINTA', 'MML lomakkeet', 'Valmiit hankkeet'); $foldersToKeep += (Get-Item -Path 'shell:::{679F85CB-0220-4080-B29B-5540CC05AAB6}').GetFolder.Items() | Select-Object -Last 5 | ForEach-Object { $_.Path }; $shellApp = New-Object -ComObject shell.application; $quickAccess = $shellApp.Namespace(0x417); $quickAccessItems = $quickAccess.Items(); foreach ($item in $quickAccessItems) { $folderPath = $quickAccess.GetFolder.Path; $folderName = $folderPath.Split('\')[-1]; if ($foldersToKeep -notcontains $folderPath) { $quickAccess.Delete($folderPath); Write-Host 'Unpinned folder: ' $folderPath; } } $shellApp.Namespace('shell:::{679F85CB-0220-4080-B29B-5540CC05AAB6}').Self.InvokeVerb('Refresh'); Write-Host 'Quick Access refreshed.' }"
+pause
+
